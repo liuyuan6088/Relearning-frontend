48c7bae953ee9ab3200a7e566fb686cd3c37eef1
liuyuan <939692178@qq.com>
2019-04-29 16:28:47 +0800

    feat:new

---
+++ js/发布者订阅模式/index.js
@@ -0,0 +1,40 @@
+
+
+class Observes {
+
+  constructor() {
+    this.list = {};
+  }
+
+  on(type, fn) {
+    if (!this.list[type]) {
+      this.list[type] = [];
+    }
+    this.list[type].push(fn);
+  }
+
+  emit(type, ...rest) {
+    if (!this.list[type]) {
+      throw new Error('错误参数')
+    }
+    const fns = this.list[type];
+    if (fns.length === 0) return false;
+    fns.forEach(e => e.apply(this, rest));
+  }
+
+  remove(type, fn) {
+    if (!this.list[type]) {
+      throw new Error('错误参数')
+    }
+    if (!fn) this.list[type] = [];
+    this.list[type].filter(e => e !== fn);
+  }
+}
+
+const observerable = new Observes();
+observerable.on('log', () => { console.log('0') });
+observerable.on('log', () => { console.log('1') });
+observerable.on('log', () => { console.log('2') });
+observerable.emit('log');
+observerable.remove('log');
+observerable.emit('log');
