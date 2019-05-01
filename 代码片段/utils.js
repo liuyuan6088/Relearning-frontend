c37ad33c4de701a1249f76946abfef37f2cddc6d
liuyuan <939692178@qq.com>
2019-05-01 21:41:34 +0800

    feat:new

--- 代码片段/utils.js
+++ 代码片段/utils.js
@@ -32,3 +32,21 @@ const camelize = cached((str) => {
 })
 
 // camelize('hello-world') // "helloWorld"
+
+const deepClone = (obj) => {
+  let objClone = Array.isArray(obj) ? [] : {};
+  if(obj && typeof obj === "object") {
+    for(let key in obj) {
+      if(obj.hasOwnProperty(key)) {
+        // 判断 obj 子元素是否为对象，如果是，递归复制
+        if(obj[key] && typeof obj[key] === "object") {
+          objClone[key] = deepClone(obj[key]);
+        } else {
+          // 如果不是，简单复制
+          objClone[key] = obj[key];
+        }
+      }
+    }
+  }
+  return objClone;
+}
